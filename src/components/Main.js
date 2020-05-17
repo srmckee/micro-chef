35fb55eb45a385f5b377953f1947a507506f1dc1
Spencer McKee <srmckee@uw.edu>
2020-05-17 15:54:28 -0700

    Added partial header and misc components

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+import Recipes from './Recipes'
+import Button from '@material-ui/core/Button';
+
+const Main = () => {
+    return (
+        <React.Fragment>
+            <Header />
+            {/*<Recipes />*/}
+        </React.Fragment>
+    )
+}
+
+export default Main;
