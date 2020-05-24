2419a1c13557fe3d3b6e74e9527fa64fea01afb4
Spencer McKee <srmckee@uw.edu>
2020-05-24 14:11:59 -0700

    header color, shape, and images

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,37 +5,75 @@ import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from "@material-ui/core/Menu"
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/styles";
+import withStyles from "@material-ui/core/styles/withStyles";
+import PropTypes from 'prop-types';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import ThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
+
+import logo from "../assets/logo.png"
+import mustache from "../assets/mustache.png"
 
 //{/*className={classes.menuButton}*/}
 //{/*className={classes.title}*/}
 
 //import "../styles.css";
 
-const useStyles = makeStyles({
-    root: {
-        color: 'red',
+const styles = {
+    rounded: {
+        borderRadius: 32,
     },
-});
+};
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#E3D5BF',
+        },
+        secondary: green,
+    },
+    shape: {
+        square: false,
+        borderRadius: 4,
+    },
+    overrides: {
+        MuiPaper: {
+            rounded: false,
+        },
+    },
+})
 
-const Header = (props) => {
+function HigherOrderComponent(props) {
     const { classes } = props;
+    return <ThemeProvider theme={theme}>
+        <Header className={classes.rounded} ></Header>;
+    </ThemeProvider>
+}
+function Header(props) {
     return (
-        <AppBar position="static">
-            <Toolbar>
-                <IconButton edge="start" color="inherit" aria-label="menu">
+        <AppBar position="static" square={false} style={{borderRadius: 20, width: "98%", maxHeight: "4%", margin: "1% auto auto"}}>
+            <Toolbar style={{display: "flex", justifyContent: "space-between"}}>
+                {/*<IconButton edge="start" color="inherit" aria-label="menu">
                     <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" >
-                    News
-                </Typography>
+                </IconButton>*/}
+                <div style={{justifyContent: "flex-start", flexWrap: "nowrap", display: "inline-block"}}>
+                  <img src={logo} alt="Logo" style={{position: "absolute", top: "-0%", left: "0%", zIndex: 10/*maxHeight: "100%"*/}}/>
+                  <Typography variant="h6">
+                    MicroChef
+                  </Typography>
+                </div>
+                <div style={{justifyContent: "flex-end", flexWrap: "nowrap", display: "inline-block"}}>
                 <Button color="inherit">Login</Button>
+                <Button color="inherit">About</Button>
+                <img src={mustache} alt="Mustache" style={{maxHeight: "100%"}} />
+                </div>
             </Toolbar>
         </AppBar>
-    )
+    );
 }
 
-export default function Hook() {
-    const classes = useStyles();
-    return <Header className={classes.root}>Test</Header>;
-}
+HigherOrderComponent.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(HigherOrderComponent);
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import Test from './Test'
+//import Test from './Test'
 import Header from './Header'
 //import Recipes from './Recipes'
-
+//import Header2 from './Header2'
 const Main = () => {
     return (
         <React.Fragment>
