44dc9e90d5d36b655f54e1edd4cf6b23bae5ea10
Spencer McKee <srmckee@uw.edu>
2020-05-25 19:29:40 -0700

    Added recipe cards

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,12 @@
 import React from 'react'
-//import Test from './Test'
 import Header from './Header'
-//import Recipes from './Recipes'
-//import Header2 from './Header2'
+import Recipes from './Recipes'
+
 const Main = () => {
     return (
         <React.Fragment>
             <Header />
-            {/*<Recipes />*/}
+            <Recipes />
         </React.Fragment>
     )
 }
--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,1 +1,60 @@
-//import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Recipe from "./Recipe";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        width: "75%",
+        margin: "auto"
+    },
+    recipe: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
+
+export default function Recipes() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+                <Grid item xs>
+                    <Recipe className={classes.recipe}>xs</Recipe>
+                </Grid>
+                <Grid item xs>
+                    <Recipe className={classes.recipe}>xs</Recipe>
+                </Grid>
+                <Grid item xs>
+                    <Recipe className={classes.recipe}>xs</Recipe>
+                </Grid>
+            </Grid>
+            <Grid container spacing={3}>
+                <Grid item xs>
+                    <Recipe className={classes.recipe}>xs</Recipe>
+                </Grid>
+                <Grid item xs>
+                    <Recipe className={classes.recipe}>xs</Recipe>
+                </Grid>
+                <Grid item xs>
+                    <Recipe className={classes.recipe}>xs</Recipe>
+                </Grid>
+            </Grid>
+            <Grid container spacing={3}>
+                <Grid item xs>
+                    <Recipe className={classes.recipe}>xs</Recipe>
+                </Grid>
+                <Grid item xs>
+                    <Recipe className={classes.recipe}>xs</Recipe>
+                </Grid>
+                <Grid item xs>
+                    <Recipe className={classes.recipe}>xs</Recipe>
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
