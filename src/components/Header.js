7916147a6ba065da14018994ae3cf01d9e268538
Yolanda Cao <yycao@uw.edu>
2020-05-25 17:48:48 -0700

    1

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,21 +9,15 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import PropTypes from 'prop-types';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import ThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
-import green from "@material-ui/core/colors/green";
-import "../fonts/Coiny/Coiny-Regular.ttf";
+import '../index.css'; //import index css to get font
 import logo from "../assets/logo.png";
 import mustache from "../assets/mustache.png";
-
-//{/*className={classes.menuButton}*/}
-//{/*className={classes.title}*/}
-
-import "../index.css";
 
 const styles = {
     root: {
         fontFace: {
             fontFamily: 'Coiny',
-            src: "local('Coiny') url(../fonts/Coiny-Regular.ttf) format('truetype')"
+            src: "local('Coiny'), url(../fonts/Coiny/Coiny-Regular.ttf) format('truetype')"
         }
     }
 };
@@ -33,15 +27,14 @@ const theme = createMuiTheme({
         primary: {
             main: '#E3D5BF',
         },
-        secondary: green,
+        secondary: {main: '#000000'},
     },
     shape: {
         square: false,
         borderRadius: 4,
     },
     typography: {
-        fontsize: 17,
-        fontFamily: 'Coiny',
+        /*fontFamily: 'Coiny',*/
       },
 
 })
@@ -54,21 +47,21 @@ function HigherOrderComponent(props) {
 }
 function Header(props) {
     return (
-        <AppBar position="static" square={false} style={{borderRadius: 20, width: "98%", height: 80, margin: "3% auto auto"}}>
+        <AppBar position="static" square={false} style={{ borderRadius: 20, width: "98%", height: 80, margin: "3% auto auto"}}>
             <Toolbar style={{display: "inline-flex", justifyContent: "space-between"}}>
                 {/*<IconButton edge="start" color="inherit" aria-label="menu">
                     <MenuIcon />
                 </IconButton>*/}
                 <img src={logo} alt="Logo"  style={{position: "absolute", top: "-30%", left: "1.5%", width:77}} />
                 <div style={{justifyContent: "flex-start", /*flexWrap: "nowrap",*/ display: "inline-block"}}>
-                  <Typography variant="h6" style={{position: "relative", top: "30%", left: "100%"}}>
+                  <Typography  variant="h6" style={{fontFamily:'Coiny', fontSize: 30, position: "relative", top: 12, left: "50%"}}>
                     MicroChef
                   </Typography>
                 </div>
                 <div style={{justifyContent: "flex-end", flexWrap: "nowrap", display: "inline-block"}}>
-                <Button color="inherit">Login</Button>
-                <Button color="inherit">About</Button>
-                <img src={mustache} alt="Mustache" style={{maxHeight: "100%"}} />
+                <Button  style={{color: "white", fontSize: 30,position: "relative",left: -10}}>LOGIN</Button>
+                <Button  style={{color: "white", fontSize: 30,position: "relative",left: -10}}>ABOUT</Button>
+                <img src={mustache} alt="Mustache" style={{position:"relative",height: 40, top:15}} />
                 </div>
             </Toolbar>
         </AppBar>
