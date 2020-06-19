0dda1438098a74f940f06544fd39df3b11768afc
Daniel Obichie <danielife4@gmail.com>
2020-06-19 18:08:39 -0400

    Fixed bug with recipe instructions not showing after optimizing Recipepage.js

--- src/components/RecipePage.js
+++ src/components/RecipePage.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import RecipeTitle from "./RecipeTitle";
 import RecipePrepCookInfo from "./RecipePrepCookInfo";
 import RecipeIngredients from "./RecipeIngredients";
@@ -7,26 +7,34 @@ import RecipeInstructions from "./RecipeInstructions";
 import getRecipeInfoPage from "../lib/getRecipeInfoPage";
 
 export default function RecipePage(props) {
-    let recipeid = props.match.params.recipeid; //473967
-    const [recipeData, setRecipeData] = useState(null);  // a recipe
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-        async function getData() {
-            setIsLoading(true);
-            await getRecipeInfoPage(recipeid).then((recipe) => {
-                setRecipeData(recipe.data.data.recipesListOfficials[0]);
-                setIsLoading(false);
-            });
-        }
-        getData();
-    }, [recipeid]);
-    return (
-        ((!isLoading) && <React.Fragment>
-        <RecipePic data={recipeData.image}/>
-        <RecipeTitle data={recipeData.title}/>
-        <RecipePrepCookInfo data={{prepTime: recipeData.preparationMinutes, cookTime: recipeData.cookingMinutes, price: recipeData.pricePerServing}}/>
-        <RecipeIngredients data={recipeData.extendedIngredients}/>
-        <RecipeInstructions data={recipeData.analyzedInstructions}/>
-    </React.Fragment>)
+  let recipeid = props.match.params.recipeid; //473967
+  const [recipeData, setRecipeData] = useState(null); // a recipe
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    async function getData() {
+      setIsLoading(true);
+      await getRecipeInfoPage(recipeid).then((recipe) => {
+        setRecipeData(recipe.data.data.recipesListOfficials[0]);
+        setIsLoading(false);
+      });
+    }
+    getData();
+  }, [recipeid]);
+  return (
+    !isLoading && (
+      <React.Fragment>
+        <RecipePic data={recipeData.image} />
+        <RecipeTitle data={recipeData.title} />
+        <RecipePrepCookInfo
+          data={{
+            prepTime: recipeData.preparationMinutes,
+            cookTime: recipeData.cookingMinutes,
+            price: recipeData.pricePerServing,
+          }}
+        />
+        <RecipeIngredients data={recipeData.extendedIngredients} />
+        <RecipeInstructions data={recipeData.analyzedInstructions[0].steps} />
+      </React.Fragment>
+    )
   );
-};
+}
